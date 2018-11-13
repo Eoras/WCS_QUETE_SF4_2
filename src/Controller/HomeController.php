7353141457e66c123804fd5dbd34abbfd1c17fe0
Paul DOS SANTOS <pauldsb@outlook.fr>
2018-11-13 16:15:15 +0100

    add biderectionnal)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +14,32 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+        $categories = $em->getRepository(Category::class)->findAll();
+
+        if(count($categories) <= 0) {
+            for ($i = 0; $i < 3; $i++) {
+                $category = new Category();
+                $category->setName('Category ' . $i);
+                $em->persist($category);
+            }
+            $em->flush();
+            $categories = $em->getRepository(Category::class)->findAll();
+
+            for ($i = 0; $i < 10; $i++) {
+                $article = new Article();
+                $article->setTitle("Title " . $i);
+                $article->setContent("Content " . $i);
+                $article->setCategory($categories[array_rand($categories)]);
+                $em->persist($article);
+            }
+            $em->flush();
+        }
+
+
+        return $this->render('home.html.twig', [
+            'catagories' => $categories
+        ]);
     }
 }
