6f66def3290cd1a23ea381e92b8616904fb19577
Paul DOS SANTOS <pauldsb@outlook.fr>
2018-11-16 16:16:57 +0100

    sf_q8

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,25 +2,104 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/blog", name="blog_")
- */
 class BlogController extends AbstractController
 {
+
     /**
-     * @Route("/{slug<([a-z0-9-]*)>}", name="show")
+     * @Route("/blog", name="blog_index")
      */
-    public function show(string $slug)
+    public function index()
     {
-        $slug = ucwords(
-            str_replace("-", " ", $slug)
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles]
+        );
+    }
+
+    /**
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/blog/{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     *  @return Response A response instance
+     */
+    public function show(string $slug): Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
         );
 
-        return $this->render('blog/show.html.twig', [
-            'slug' => $slug,
-        ]);
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with ' . $slug . ' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+                'slug' => $slug,
+            ]
+        );
     }
+
+
+    /**
+     * @param string $category
+     * @return Response
+     * @Route("/blog/category/{category}", name="blog_show_category")
+     */
+    public function showByCategory(string $category)
+    {
+        $category = $this->getDoctrine()
+            ->getRepository(Category::class)
+            ->findOneBy(["name" => $category]);
+
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findBy(['category' => $category], ['id' => "DESC"], 3);
+        dump($articles);
+
+        if($category) {
+            return $this->render("blog/category.html.twig", [
+                'category' => $category,
+                'articles' => $articles
+            ]);
+        }
+
+        return $this->redirectToRoute('blog_index');
+
+    }
+
+
 }
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,36 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
-
-        $em = $this->getDoctrine()->getManager();
-        $categories = $em->getRepository(Category::class)->findAll();
-
-        if(count($categories) <= 0) {
-            for ($i = 0; $i < 3; $i++) {
-                $category = new Category();
-                $category->setName('Category ' . $i);
-                $em->persist($category);
-            }
-            $em->flush();
-            $categories = $em->getRepository(Category::class)->findAll();
-
-            for ($i = 0; $i < 10; $i++) {
-                $article = new Article();
-                $article->setTitle("Title " . $i);
-                $article->setContent("Content " . $i);
-                $article->setCategory($categories[array_rand($categories)]);
-                $em->persist($article);
-            }
-            $em->flush();
-        }
-
-
-        return $this->render('home.html.twig', [
-            'catagories' => $categories
-        ]);
+        return $this->render('home.html.twig');
     }
 }
